88350c35f51f9077ba2029761deb546593c1c314
Rinigus <rinigus.git@gmail.com>
2021-02-23 17:23:45 +0200

    drop excessive logs recorded on boot

--- sparse/root/logs.sh
+++ sparse/root/logs.sh
@@ -4,7 +4,7 @@ journalctl -b > /root/journal.$1.log
 df -a -h > /root/df.$1.log
 /usr/libexec/droid-hybris/system/bin/logcat -d -b all > /root/logcat.$1.log
 dmesg > /root/dmesg.$1.log
-ls -lR /config > /root/ls-lR.config.$1.log
-ls -lR /sys > /root/ls-lR.sys.$1.log
-ls -lR /dev > /root/ls-lR.dev.$1.log
-ls /proc > /root/ls.proc.$1.log
+#ls -lR /config > /root/ls-lR.config.$1.log
+#ls -lR /sys > /root/ls-lR.sys.$1.log
+#ls -lR /dev > /root/ls-lR.dev.$1.log
+#ls /proc > /root/ls.proc.$1.log
